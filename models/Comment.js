8c746a46fad0b4c2de3bda300976df41cee10cff
zachary chapman <zach.chapman24@gmail.com>
2024-03-02 13:29:00 -0500

    added models

---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+const sequelize = require("../config/connection");
+const { Model, DataTypes } = require("sequelize");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comment",
+  }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,20 @@
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
+
+BlogPost.hasMany(Comment, {
+  foreignKey: 'blogPost_id'
+})
+
+
+module.exports = { User, BlogPost };
