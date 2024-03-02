52fbf5ce57e3fb1efb384b04ca5a0cd000b0f093
zachary chapman <zach.chapman24@gmail.com>
2024-03-02 14:49:35 -0500

    ser up api routes and tested they are woeking

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { BlogPost } = require('../../models/');
+const { apiGuard } = require('../../utils/authGuard');
+
+router.post('/', apiGuard, async (req, res) => {
+  const body = req.body;
+  try {
+    const newBlogPost = await BlogPost.create({ ...body, userId: req.session.user_id });
+    res.json(newBlogPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.put('/:id', apiGuard, async (req, res) => {
+  try {
+    const [affectedRows] = await BlogPost.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.delete('/:id', apiGuard, async (req, res) => {
+  try {
+    const [affectedRows] = BlogPost.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { BlogPost } = require('../models');
+const { withGuard } = require('../utils/authGuard');
+router.get('/', withGuard, async (req, res) => {
+  try {
+    const BlogPostData = await BlogPost.findAll({
+      where: {
+        userId: req.session.user_id,
+      },
+    });
+    const blogPost = BlogPostData.map((blogPost) => blogPost.get({ plain: true }));
+    res.render('dashboard', {
+      dashboard: true,
+      posts,
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/new', withGuard, (req, res) => {
+  res.render('newPost', {
+    dashboard: true,
+    loggedIn: req.session.logged_in,
+  });
+});
+router.get('/edit/:id', withGuard, async (req, res) => {
+  try {
+    const BlogPostData = await BlogPost.findByPk(req.params.id);
+    if (BlogPostData) {
+      const blogPost = BlogPostData.get({ plain: true });
+      res.render('editBlogPost', {
+        dashboard: true,
+        blogPost,
+        loggedIn: req.session.logged_in,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
